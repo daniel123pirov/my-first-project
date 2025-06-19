d09f55713626aecd54f1de0542925e8b2ee3b28c
daniel pirov <d0548537383@gmail.com>
2025-06-19 19:28:35 +0300

    add factorial

--- math.functionc.py
+++ math.functionc.py
@@ -1,7 +1,14 @@
 def welcome():
     """Welcome function"""
-
-
-    print("Welcome to the algorithmic functions library!")
 if __name__ == "__main__":
     welcome()
+    print("Welcome to the algorithmic functions library!")
+def factorial(n):
+    """Function for factorial"""
+    if n==1 or n ==0:
+        return 1
+    return n* factorial(n-1)
+if __name__ == "__main__":
+    factorial(5)
+
+
